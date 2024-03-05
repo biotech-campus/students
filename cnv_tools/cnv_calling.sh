b9b2c6a0171c2ad184a7286d4444db309aaf0fe9
ayamoncheryaev <jamoncheryev1@gmail.com>
2024-03-05 19:39:51 +0000

    cnv: 3 tools running on bam

--- cnv_tools/cnv_calling.sh
+++ cnv_tools/cnv_calling.sh
@@ -1,24 +1,62 @@
 docker build -t cnv_calling:latest .
 
-# CNV
 CONTAINER="cnv_calling:latest"
 TOOL="$1"
 WORKDIR="/home"
+CPU=6
 
 # Input
 REF_DIR="/mnt/data/common/hg38"
-IN_DIR="/mnt/data/ayamoncheryaev/"
+IN_DIR="/mnt/data/ayamoncheryaev/input"
+BAM_FILE="HG002_GRCh38_ONT-UL_GIAB_20200204" # Long reads
+# BAM_FILE="HG002.GRCh38.2x250.bam" # Short reads
+
+# Output
 OUT_DIR="/mnt/data/ayamoncheryaev/${TOOL}"
-BAMFILE="/HG002_GRCh38_ONT-UL_GIAB_20200204.bam"
-# BAMFILE="/HG002.GRCh38.2x250.bam"
-CPU=4
 
-CNVPYTOR_COMMAND=--bam="${WORKDIR}/input/${BAMFILE}"
+VOLUME_OPTIONS="--volume ${REF_DIR}:${WORKDIR}/reference:ro \
+        --volume ${IN_DIR}:${WORKDIR}/input:ro \
+        --volume ${OUT_DIR}:${WORKDIR}/output"
 
-docker run \
-    --volume ${REF_DIR}:"${WORKDIR}/reference":ro \
-    --volume ${IN_DIR}:"${WORKDIR}/input":ro \
-    --volume ${OUT_DIR}:"${WORKDIR}/output" \
-    --workdir ${WORKDIR} \
-    ${CONTAINER} \
-        python3 script.py ${TOOL} ${CNVPYTOR_COMMAND}
+
+if [ "${TOOL}" = "cnvpytor" ]
+then 
+    docker run --rm \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            python3 script.py ${TOOL} \
+            --bam ${WORKDIR}/input/${BAM_FILE}.bam \
+            -o ${WORKDIR}/output \ 
+            --cpu ${CPU}
+fi
+
+if [ "${TOOL}" = "cnvkit" ]
+then 
+    docker run \
+        --cpus=${CPU} \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            cnvkit.py batch \
+                ${WORKDIR}/input/${BAM_FILE} -n \
+                -f ${WORKDIR}/reference/hg38.fa \
+                --access ${WORKDIR}/output/access.hg38.bed \
+                --output-reference ${WORKDIR}/output/flat_reference.cnn \
+                -d ${WORKDIR}/output/ \
+                -m wgs \
+                -p ${CPU}
+fi
+
+
+if [ "${TOOL}" = "sniffles" ]
+then 
+    docker run \
+        --cpus=${CPU} \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            sniffles --input ${WORKDIR}/input/${BAM_FILE}.bam \
+                --vcf ${WORKDIR}/output/${BAM_FILE}.vcf \
+                --snf ${WORKDIR}/output/${BAM_FILE}.snf \
+                --threads ${CPU} \
+                --reference ${WORKDIR}/reference/hg38.fa \
+                --allow-overwrite            
+fi
--- cnv_tools/script.py
+++ cnv_tools/script.py
@@ -1,33 +1,48 @@
 from cnvpytor.root import Root
 import argparse
 import logging
+import pandas as pd
+
 
 def cnvpytor(args):
-    logger = logging.getLogger(args.tool)
-    app = Root(args.pytor, create=True, max_cores=args.cpu)
+    bam_name = args.bam.split('/')[-1].split('.')[0] # use bam-file name to name cache files 
+    app = Root(f'{args.output_dir}/{bam_name}.pytor', create=True, max_cores=args.cpu)
+
+    # Calling CNVs
     app.rd([args.bam])
     app.calculate_histograms(args.hists)
     app.partition(args.hists)
-    calls = app.call(args.hists, print_calls=True)
+    calls = app.call(args.hists)
+
+    #Write to output file
+    for bin_size in calls:
+        pd.DataFrame(calls[bin_size]).to_csv(f'{args.output_dir}/{bam_name}_h{bin_size}.csv')
 
 
 def cnvkit(args):
     raise NotImplementedError('CNVkit calling is not implemented yet!')
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('tool', type=str, help='Tool name to launch')
     parser.add_argument('--bam', '-b', type=str, help='BAM filename', required=True)
-    parser.add_argument('--pytor', '-n', type=str, help='CNVpytor filename', default='cache.pytor')
+    parser.add_argument('-o', '--output-dir', type=str, help='Dir for output files', required=True)
+    
     parser.add_argument('--hists', nargs='+', help=' List of histogram bin sizes', default=[10000])
     parser.add_argument('--cpu', type=int, help='Max CPUs to use', default=4)
     args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(args.tool)
 
     if args.tool == 'cnvpytor':
         cnvpytor(args)
+
+        
     elif args.tool == 'cnvkit':
         cnvkit(args)
+
 
 if __name__ == '__main__':
     main()
