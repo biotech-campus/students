ce8b3b2914873d6a9fc27037e5ea949a68733a06
ayamoncheryaev <jamoncheryev1@gmail.com>
2024-04-04 09:29:29 +0000

    cnv: only long reads, new tool spectre, vcf compare

--- cnv_tools/cnv_calling.sh
+++ cnv_tools/cnv_calling.sh
@@ -10,6 +10,8 @@ REF_DIR="/mnt/data/common/hg38"
 IN_DIR="/mnt/data/ayamoncheryaev/input"
 BAM_FILE="HG002_GRCh38_ONT-UL_GIAB_20200204" # Long reads
 # BAM_FILE="HG002.GRCh38.2x250.bam" # Short reads
+TARGET_GRCh38_BED=GRCh38.d1.vd1.main.bed
+CHROMS=$(cat ${REF_DIR}/chroms.txt)
 
 # Output
 OUT_DIR="/mnt/data/ayamoncheryaev/${TOOL}"
@@ -18,69 +20,57 @@ VOLUME_OPTIONS="--volume ${REF_DIR}:${WORKDIR}/reference:ro \
         --volume ${IN_DIR}:${WORKDIR}/input:ro \
         --volume ${OUT_DIR}:${WORKDIR}/output"
 
-
 if [ "${TOOL}" = "cnvpytor" ]
 then 
-    docker run --rm \
+    docker run \
         ${VOLUME_OPTIONS} \
         ${CONTAINER} \
-            python3 script.py ${TOOL} \
-            --bam ${WORKDIR}/input/${BAM_FILE}.bam \
-            -o ${WORKDIR}/output \ 
-            --cpu ${CPU}
+            python3 cnvpytor_launch.py \
+                --bam ${WORKDIR}/input/${BAM_FILE}.bam \
+                -o ${WORKDIR}/output \
+                --cpu ${CPU} \
+                --chroms ${CHROMS}
 fi
 
-if [ "${TOOL}" = "cnvkit" ]
+if [ "${TOOL}" = "spectre" ]
+then 
+    WINDOW_SIZE=1000
+    docker run \
+        --cpus=${CPU} \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            sh -c "
+                mosdepth \
+                    -x -n \
+                    -t ${CPU} \
+                    -Q 20 \
+                    -b 1000 \
+                    ${WORKDIR}/output/${BAM_FILE} \
+                    ${WORKDIR}/input/${BAM_FILE}.bam \
+                && \
+                spectre CNVCaller \
+                    --threads ${CPU} \
+                    --coverage ${WORKDIR}/output/${BAM_FILE}.regions.bed.gz \
+                    --sample-id ${BAM_FILE} \
+                    --output-dir ${WORKDIR}/output/spectre_output/ \
+                    --reference ${WORKDIR}/reference/hg38.fa \
+                    --min-cnv-len 80000 \
+                    --blacklist ${WORKDIR}/input/data/grch38_blacklist.bed\
+                    --metadata ${WORKDIR}/input/data/grch38.mdr \
+                    --only-chr ${CHROMS}"
+fi
+                
+if [ "${TOOL}" = "vcftoolz" ]
 then 
     docker run \
         --cpus=${CPU} \
         ${VOLUME_OPTIONS} \
         ${CONTAINER} \
-            cnvkit.py batch \
-                ${WORKDIR}/input/${BAM_FILE} -n \
-                -f ${WORKDIR}/reference/hg38.fa \
-                --access ${WORKDIR}/output/access.hg38.bed \
-                --output-reference ${WORKDIR}/output/flat_reference.cnn \
-                -d ${WORKDIR}/output/ \
-                -m wgs \
-                -p ${CPU}
-fi
-
-
-if [ "${TOOL}" = "sniffles" ]
-then 
-    docker run \
-        --cpus=${CPU} \
-        ${VOLUME_OPTIONS} \
-        ${CONTAINER} \
-            sniffles --input ${WORKDIR}/input/${BAM_FILE}.bam \
-                --vcf ${WORKDIR}/output/${BAM_FILE}.vcf \
-                --snf ${WORKDIR}/output/${BAM_FILE}.snf \
-                --threads ${CPU} \
-                --reference ${WORKDIR}/reference/hg38.fa \
-                --allow-overwrite            
-fi
-
-if [ "${TOOL}" = "svim" ]
-then 
-    docker run \
-        --cpus=${CPU} \
-        ${VOLUME_OPTIONS} \
-        ${CONTAINER} \
-            svim alignment \
-                ${WORKDIR}/output \
-                ${WORKDIR}/input/${BAM_FILE}.bam \
-                ${WORKDIR}/reference/hg38.fa  \
-                --min_mapq 14 \
-                --min_sv_size 1000 \
-                --types=DEL,INS,DUP:TANDEM 
-fi
-
-if [ "${TOOL}" = "gatk" ]
-then 
-    docker pull broadinstitute/gatk:latest
-    docker run -it --rm \
-        --cpus=${CPU} \
-        ${VOLUME_OPTIONS} \
-        broadinstitute/gatk
+            sh -c "
+            cd ${WORKDIR}/output && \
+            vcftoolz compare \
+                ${WORKDIR}/output/HG002_GRCh38_ONT-UL_GIAB_20200204_10000.vcf.gz \
+                ${WORKDIR}/output/HG002_GRCh38_ONT-UL_GIAB_20200204.vcf.gz \
+                ${WORKDIR}/output/HG002.GRCh38.deepvariant.g.vcf.gz \
+                > ${WORKDIR}/output/summary.txt"
 fi
--- cnv_tools/cnvpytor_launch.py
+++ cnv_tools/cnvpytor_launch.py
@@ -1,4 +1,5 @@
 from cnvpytor.root import Root
+from cnvpytor.viewer import Viewer
 import argparse
 import logging
 import pandas as pd
@@ -9,42 +10,44 @@ def cnvpytor(args):
     app = Root(f'{args.output_dir}/{bam_name}.pytor', create=True, max_cores=args.cpu)
 
     # Calling CNVs
-    app.rd([args.bam])
-    app.calculate_histograms(args.hists)
-    app.partition(args.hists)
-    calls = app.call(args.hists)
+    chroms = args.chroms
+    if len(chroms) == 1 and isinstance(chroms, list):
+        chroms = chroms[0].split(',')
+    app.rd([args.bam], chroms=chroms)
+    app.calculate_histograms(args.hists, chroms=chroms)
+    app.partition(args.hists, chroms=chroms)
+    calls = app.call(args.hists, chroms=chroms)
 
-    #Write to output file
+    # Write to CSV file
     for bin_size in calls:
         pd.DataFrame(calls[bin_size]).to_csv(f'{args.output_dir}/{bam_name}_h{bin_size}.csv',
                                              index=False,
                                              header=['id', 'type', 'start', 'end', 'size', 'cnv', 'p_val',
                                                      'p_val_2', 'p_val_3', 'p_val_4', 'Q0', 'pN', 'dG'])
-
-def cnvkit(args):
-    raise NotImplementedError('CNVkit calling is not implemented yet!')
+    # Write to VCF file
+    for bin_size in args.hists:
+        viewer = Viewer([f'{args.output_dir}/{bam_name}.pytor'],
+                        params={'print_filename': f'{args.output_dir}/{bam_name}_{bin_size}.vcf',
+                                'annotate': True,
+                                'bin_size': bin_size})
+        viewer.print_calls_file()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('tool', type=str, help='Tool name to launch')
     parser.add_argument('--bam', '-b', type=str, help='BAM filename', required=True)
     parser.add_argument('-o', '--output-dir', type=str, help='Dir for output files', required=True)
     
     parser.add_argument('--hists', nargs='+', help=' List of histogram bin sizes', default=[10000])
+    parser.add_argument('--chroms', nargs='+', help=' List of chromosomes used in analysis', default=[])
     parser.add_argument('--cpu', type=int, help='Max CPUs to use', default=4)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(args.tool)
+    logger = logging.getLogger('cnvpytor')
 
-    if args.tool == 'cnvpytor':
-        cnvpytor(args)
-
-
-    elif args.tool == 'cnvkit':
-        cnvkit(args)
-
+    cnvpytor(args)
+        
 
 if __name__ == '__main__':
     main()
