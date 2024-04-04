16e2edb829240003a49101c209b25ac4340347cd
ayamoncheryaev <jamoncheryev1@gmail.com>
2024-04-04 11:13:58 +0000

    cnv: change volumes, return cnvkit and svim

--- cnv_tools/cnv_calling.sh
+++ cnv_tools/cnv_calling.sh
@@ -16,9 +16,10 @@ CHROMS=$(cat ${REF_DIR}/chroms.txt)
 # Output
 OUT_DIR="/mnt/data/ayamoncheryaev/${TOOL}"
 
-VOLUME_OPTIONS="--volume ${REF_DIR}:${WORKDIR}/reference:ro \
-        --volume ${IN_DIR}:${WORKDIR}/input:ro \
-        --volume ${OUT_DIR}:${WORKDIR}/output"
+VOLUME_OPTIONS="--volume ${REF_DIR}:${REF_DIR}:ro \
+        --volume ${IN_DIR}:${IN_DIR}:ro \
+        --volume ${OUT_DIR}:${OUT_DIR}"
+
 
 if [ "${TOOL}" = "cnvpytor" ]
 then 
@@ -26,11 +27,12 @@ then
         ${VOLUME_OPTIONS} \
         ${CONTAINER} \
             python3 cnvpytor_launch.py \
-                --bam ${WORKDIR}/input/${BAM_FILE}.bam \
-                -o ${WORKDIR}/output \
+                --bam ${IN_DIR}/${BAM_FILE}.bam \
+                -o ${OUT_DIR} \
                 --cpu ${CPU} \
                 --chroms ${CHROMS}
 fi
+
 
 if [ "${TOOL}" = "spectre" ]
 then 
@@ -45,21 +47,22 @@ then
                     -t ${CPU} \
                     -Q 20 \
                     -b 1000 \
-                    ${WORKDIR}/output/${BAM_FILE} \
-                    ${WORKDIR}/input/${BAM_FILE}.bam \
+                    ${OUT_DIR}/${BAM_FILE} \
+                    ${IN_DIR}/${BAM_FILE}.bam \
                 && \
                 spectre CNVCaller \
                     --threads ${CPU} \
-                    --coverage ${WORKDIR}/output/${BAM_FILE}.regions.bed.gz \
+                    --coverage ${OUT_DIR}/${BAM_FILE}.regions.bed.gz \
                     --sample-id ${BAM_FILE} \
-                    --output-dir ${WORKDIR}/output/spectre_output/ \
-                    --reference ${WORKDIR}/reference/hg38.fa \
+                    --output-dir ${OUT_DIR}/spectre_output/ \
+                    --reference ${REF_DIR}/hg38.fa \
                     --min-cnv-len 80000 \
-                    --blacklist ${WORKDIR}/input/data/grch38_blacklist.bed\
-                    --metadata ${WORKDIR}/input/data/grch38.mdr \
+                    --blacklist ${IN_DIR}/data/grch38_blacklist.bed\
+                    --metadata ${IN_DIR}/data/grch38.mdr \
                     --only-chr ${CHROMS}"
 fi
-                
+
+
 if [ "${TOOL}" = "vcftoolz" ]
 then 
     docker run \
@@ -67,10 +70,43 @@ then
         ${VOLUME_OPTIONS} \
         ${CONTAINER} \
             sh -c "
-            cd ${WORKDIR}/output && \
+            cd ${OUT_DIR}/pbsv_phased && \
             vcftoolz compare \
-                ${WORKDIR}/output/HG002_GRCh38_ONT-UL_GIAB_20200204_10000.vcf.gz \
-                ${WORKDIR}/output/HG002_GRCh38_ONT-UL_GIAB_20200204.vcf.gz \
-                ${WORKDIR}/output/HG002.GRCh38.deepvariant.g.vcf.gz \
-                > ${WORKDIR}/output/summary.txt"
+                ${OUT_DIR}//HG002_GRCh38_ONT-UL_GIAB_20200204_10000.vcf.gz \
+                ${OUT_DIR}/HG002_GRCh38_ONT-UL_GIAB_20200204.vcf.gz \
+                ${OUT_DIR}/HG002.GRCh38.pbsv.phased.vcf.gz \
+                > ${OUT_DIR}/pbsv_phased/summary.txt"
+fi
+
+
+if [ "${TOOL}" = "svim" ]
+then 
+    docker run \
+        --cpus=${CPU} \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            svim alignment \
+                ${OUT_DIR} \
+                ${IN_DIR}/${BAM_FILE}.bam \
+                ${REF_DIR}/hg38.fa  \
+                --min_mapq 14 \
+                --min_sv_size 1000 \
+                --types=DEL,INS,DUP:TANDEM 
+fi
+
+
+if [ "${TOOL}" = "cnvkit" ]
+then 
+    docker run \
+        --cpus=${CPU} \
+        ${VOLUME_OPTIONS} \
+        ${CONTAINER} \
+            cnvkit.py batch \
+                ${IN_DIR}/${BAM_FILE} -n \
+                -f ${REF_DIR}/hg38.fa \
+                --access ${OUT_DIR}/access.hg38.bed \
+                --output-reference ${OUT_DIR}/flat_reference.cnn \
+                -d ${OUT_DIR}/ \
+                -m wgs \
+                -p ${CPU}
 fi
